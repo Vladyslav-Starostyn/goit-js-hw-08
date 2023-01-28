6ed8fd473c00b8d4b36408f003538b34efdec0bd
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2023-01-29 01:13:07 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,47 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const itemsMarkup = createItemMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createItemMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    )
+    .join('');
+}
+
+galleryEl.addEventListener('click', onItemClick);
+
+function onItemClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  galleryEl.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  });
+}
